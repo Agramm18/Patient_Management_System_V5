4ef96466127014acf8be65ace69dc35fe2e132a5
Alex <grammaleks@gmail.com>
2026-02-27 15:34:31 +0100

    Added basic collector to get date values

--- AddPatient.java
+++ AddPatient.java
@@ -9,6 +9,7 @@ public class AddPatient {
     
     //Age Information
     private int age;
+    private int birthday;
     private int birthday_day;
     private int birthday_month;
     private int birthday_year;
@@ -62,11 +63,63 @@ public class AddPatient {
         }
     }
 
+
+    public void SetAge(Scanner scanner) {
+        //Set the aptients age
+        while (true) {
+            try {
+
+                String birthdayDayString;
+                String birthdayMonthString;
+                String birthdayYearString;
+
+                //Collect the values via user Input
+                System.out.println("Please type in the day from your birthday: ");
+                birthdayDayString = scanner.nextLine();
+
+                System.out.println("Please type in the month from your birthday: ");
+                birthdayMonthString = scanner.nextLine();
+
+                System.out.println("Please type in the year where you born: ");
+                birthdayYearString = scanner.nextLine();
+
+                //Check if user_input is empty
+                if (birthdayDayString.isBlank() || birthdayMonthString.isBlank() || birthdayYearString.isBlank()) {
+                    throw new IllegalArgumentException("One of your inputs are blank please try again");
+                } else {
+                    //If not parse the integer values
+                    this.birthday_day = Integer.parseInt(birthdayDayString);
+                    this.birthday_month = Integer.parseInt(birthdayMonthString);
+                    this.birthday_year = Integer.parseInt(birthdayYearString);
+
+                    //Check if User input values are valid
+                    if (this.birthday_day < 1 || this.birthday_month < 1 || this.birthday_year < 1) {
+                        throw new IllegalArgumentException("The Day/Month/Year can't be less than 1");
+                    } else if (this.birthday_year < 1900) {
+                        throw new IllegalArgumentException("Are you realy this old? the year is invalid cause it's behind 1900");
+                    } else if (this.birthday_year > LocalDate.now().getYear()) {
+                        throw new IllegalArgumentException("Are you from the future? the year is invalid cause it's from the future");
+                    } else if (this.birthday_month > 12) {
+                        throw new IllegalArgumentException("Your month is out of range only 1-12 are valid");
+                    }
+                }
+                break;
+            } catch (Exception error) {
+                System.out.println("\nThere is an error in your code");
+                System.out.println("The error is: " + error + "\n");
+            }
+        }
+    }
+
     //Display Data
     public void DisplayPatient() {
         System.out.println("\n------------------The Patient------------\n");
         System.out.println("First Name: " + this.fname);
         System.out.println("Last Name: " + this.lname);
+
+        System.out.println("Birthday day: " + this.birthday_day);
+        System.out.println("Birthday month: " + this.birthday_month);
+        System.out.println("Birthday year: " + this.birthday_year);
     }
 
     //Add to Data Base
